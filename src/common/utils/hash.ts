5ffbf6cae3838524964e636f0b440a2bd59e851e
Vu Viet Hoang <hoangcua530@gmail.com>
2026-09-12 06:06:17 +0700

    hash and compare password

---
+++ src/common/utils/hash.ts
@@ -0,0 +1,10 @@
+import bcrypt from "bcrypt";
+
+export async function hashPassword(plain: string) {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(plain, salt);
+}
+
+export async function checkPassword(plain: string, hashed: string) {
+  return bcrypt.compare(plain, hashed);
+}
