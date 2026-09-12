1d754bac27e3737ec0f691be5bd02f0655d30ade
Vu Viet Hoang <hoangcua530@gmail.com>
2026-09-12 09:10:48 +0700

    indexing modules route

--- src/index.routes.ts
+++ src/index.routes.ts
@@ -1,4 +1,8 @@
 import { Request, Response, Router } from "express";
+import authRoute from "./modules/auth/auth.routes.js";
+import userRoute from "./modules/user/user.routes.js";
+import roleRoute from "./modules/role/role.routes.js";
+import permissionRoute from "./modules/permission/permission.routes.js";
 
 const router = Router();
 
@@ -9,5 +13,10 @@ router.get("/", (req: Request, res: Response) => {
     secure: req.secure,
   });
 });
+
+router.use("/auth", authRoute);
+router.use("/users", userRoute);
+router.use("/roles", roleRoute);
+router.use("/permission", permissionRoute);
 
 export default router;
